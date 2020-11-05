d4f3ab3a74dce05111b48249e8262a1ebbba79c0
David Mohl <git@davemail.io>
2020-11-05 21:26:49 +0900

    Fix missing session in cli

--- README.md
+++ README.md
@@ -18,7 +18,7 @@ Get current channel information
 const client = new NetcastClient(API_HOST);
 client.get_session(access_token).then(async (session_id) => {
     const cur_channel = await client.get_current_channel(session_id);
-    console.log(cur_channel)
+    console.log(cur_channel);
 });
 ```
 
@@ -28,6 +28,44 @@ Check the [API docs](./docs.md) for general usage on this package
 
 To interact with the TV, you need a valid `access_token`. The access token is actually just the pair code of the TV, you can get that with `client.display_pair_code()`.
 
+## CLI
+
+This package comes with a very simple CLI to query TV information
+
+```bash
+Usage: netcast-cli [options]
+
+Options:
+  --host: Host of the TV
+    (default: "192.168.1.1:8080")
+  --access_token: Pair code of the TV
+    (default: "123456")
+    (an integer)
+  --command: command to issue
+    (default: "current_channel")
+```
+
+```bash
+❯ netcast-cli --host 192.168.1.14:8080 --access_token 1234567
+Querying current channel
+{
+  chtype: 'satellite',
+  sourceIndex: '7',
+  physicalNum: '265',
+  major: '200',
+  displayMajor: '200',
+  minor: '65535',
+  displayMinor: '-1',
+  chname: 'スターチャンネル１',
+  progName: 'エスケープ・ルーム（２０１９）',
+  audioCh: '0',
+  inputSourceName: 'TV',
+  inputSourceType: '0',
+  labelName: {},
+  inputSourceIdx: '0'
+}
+```
+
 ## License
 
-MIT 
+MIT
--- cli.ts
+++ cli.ts
@@ -1,34 +1,35 @@
 #!/usr/bin/env node
-import { NetcastClient } from "./netcast";
-import flags from "flags";
+import { NetcastClient } from './netcast';
+import flags from 'flags';
 
-flags.defineString("host", "192.168.1.1:8080", "Host of the TV");
-flags.defineInteger("access_token", "123456", "Pair code of the TV");
-flags.defineString("command", "current_channel", "command to issue");
+flags.defineString('host', '192.168.1.1:8080', 'Host of the TV');
+flags.defineInteger('access_token', '123456', 'Pair code of the TV');
+flags.defineString('command', 'current_channel', 'command to issue');
 
 flags.parse();
 
 const command_mapping = {
-    current_channel: () => {
-        console.log("Querying current channel");
-        const client = new NetcastClient(flags.get("host"));
-        client.get_current_channel().then(
-            (res) => {
-                console.log(res);
-            },
-            (err) => {
-                console.log("Error from TV: " + err);
-            }
-        );
+    current_channel: async () => {
+        console.log('Querying current channel');
+        const client = new NetcastClient(flags.get('host'));
+
+        try {
+            await client.get_session(flags.get('access_token'));
+            console.log(await client.get_current_channel());
+        } catch (error) {
+            console.log('Error from TV: ', error);
+        }
+
+        process.exit(0);
     },
     help: () => {
-        console.log("Help");
+        console.log('Help');
     },
 };
 
-const command = flags.get("command");
+const command = flags.get('command');
 if (Object.keys(command_mapping).indexOf(command) === -1) {
-    console.log("Command " + command + " not supported. Available commands:");
+    console.log('Command ' + command + ' not supported. Available commands:');
     console.log(Object.keys(command_mapping));
     process.exit(0);
 }
